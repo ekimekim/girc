68d0546a282978073d12cda4adaed842b0766062
Mike Lang <mikelang3000@gmail.com>
2014-05-24 21:09:04 -0700

    userlist: Track users and their modes in a channel

--- geventirc/userlist.py
+++ geventirc/userlist.py
@@ -34,8 +34,18 @@ class UserList(object):
 	def __init__(self, client, channel):
 		"""Takes a client object and string channel name. Begins watching for relevant messages immediately."""
 		self.client = client
+		self.channel = channel
 		self.parse_prefix_str()
-		# TODO
+		self.client.add_handler(self.recv_user_list, command=replycodes.replies.NAMREPLY,
+		                        params=lambda params: len(params) > 1 and params[1] == channel)
+		self.client.add_handler(self.user_join, command='JOIN',
+		                        channels=lambda channels: channel in channels)
+		self.client.add_handler(self.user_leave, command='PART',
+		                        channels=lambda channels: channel in channels)
+		self.client.add_handler(self.user_leave, command='KICK', channel=channel)
+		self.client.add_handler(self.user_leave, command='QUIT')
+		self.client.add_handler(self.user_mode_change, command='MODE', target=channel)
+		self.client.add_handler(self.user_nick_change, command='NICK')
 
 	def parse_prefix_str(self):
 		prefix_str = self.client.server_properties['PREFIX']
@@ -52,15 +62,15 @@ class UserList(object):
 		chars.append('')
 
 		self.modes = modes
-		self.mode_map = dict(zip(chars, modes))
-		self._user_map = {mode: set() for mode in self.modes}
+		self.prefix_map = dict(zip(chars, modes)) # prefix_map maps prefix chars to modes
+		self._user_map = {mode: set() for mode in self.modes} # user_map maps modes to users
 
 	def _resolve_name(self, name):
 		"""Returns mode given mode, prefix or friendly name, or None"""
 		if name in self.modes:
 			return name
-		if name in self.mode_map:
-			return self.mode_map[name]
+		if name in self.prefix_map:
+			return self.prefix_map[name]
 		if name in KNOWN_NAMES:
 			mode = KNOWN_NAMES[name]
 			if mode in self.modes:
@@ -108,3 +118,66 @@ class UserList(object):
 	def unregister(self):
 		"""Stop watching for relevant messages, removing the handlers from the client."""
 		# TODO
+
+	# handlers
+
+	def recv_user_list(self, msg):
+		users = msg.params[2:]
+		for raw_user in users:
+			prefix, user = raw_user[0], raw_user[1:]
+			if prefix not in self.mode_map:
+				# no prefix, normal user
+				prefix = ''
+				user = raw_user
+			mode = self.mode_map[prefix]
+			self._user_map[mode].add(user)
+
+	def user_join(self, msg):
+		user = msg.sender
+		# we might or might not already have this user under a certain mode
+		# if not, we put them in users until they get MODEed
+		if user not in self.users:
+			self._user_map[''].add(user)
+
+	def user_leave(self, msg):
+		if isinstance(msg, Kick):
+			user = msg.nick
+		else:
+			user = msg.sender
+		for user_set in self._user_map.values():
+			if user in user_set:
+				user_set.remove(user)
+
+	def user_mode_change(self, msg):
+		user = msg.arg
+
+		# get highest mode in self.modes
+		for mode in self.modes:
+			# don't count '' as a mode here
+			if mode and mode in msg.flags:
+				break
+		else:
+			return # no recognized modes
+
+		user_set = self._user_map[mode]
+		if msg.remove:
+			if user in user_set:
+				user_set.remove(user)
+			# need to add back into basic users
+			self._user_map[''].add(user)
+			# XXX this could mean we've lost knowledge of a lesser mode
+		else:
+			user_set.add(user)
+			# need to remove from any lesser modes
+			for lesser_mode in self.modes[self.modes.index(mode):]:
+				lesser_set = self._user_set[lesser_mode]
+				if user in lesser_set:
+					lesser_set.remove(user)
+
+	def user_nick_change(self, msg):
+		old_nick = msg.sender
+		new_nick = msg.nickname
+		for user_set in self._user_map.values():
+			if old_nick in user_set:
+				user_set.remove(old_nick)
+				user_set.add(new_nick)
