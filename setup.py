f46d25579f5924636d265cde656343c61cfc916b
Mike Lang <mikelang3000@gmail.com>
2014-12-14 02:02:19 -0800

    Replace README and modify setup.py, since I share almost zero code with the old project
    What to do about name conflict, since they're really different libraries at this point?

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name="geventirc",
-      version="0.1dev",
-      author="Antonin Amand (@gwik)",
-      author_email="antonin.amand@gmail.com",
-      description="gevent based irc client",
+      version="0.0.1",
+      author="ekimekim",
+      author_email="mikelang3000@gmail.com",
+      description="gevent based irc library",
       packages=find_packages(),
       install_requires=[
           'gevent',
