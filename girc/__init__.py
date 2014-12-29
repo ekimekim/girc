609a19648151d1d820ec0b53151c88f6f61205e6
Mike Lang <mikelang3000@gmail.com>
2014-12-29 02:34:15 -0800

    __init__: Import useful things so users don't need to find the right module

--- girc/__init__.py
+++ girc/__init__.py
@@ -1,1 +1,5 @@
 from girc.client import Client
+from girc.handler import Handler
+from girc.replycodes import replies, errors
+from girc.message import Message, Nick, User, Quit, Join, Part, Mode, Privmsg, List, Kick, Whois, \
+                         Ping, Pong, ISupport, match
