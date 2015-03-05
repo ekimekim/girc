fe2d611cd9925b13719dfbcd0cf2e0e327185df6
Mike Lang <mikelang3000@gmail.com>
2015-03-05 16:58:58 +0000

    setup: Re-write to use indented lines rather than aligned to first line

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
-setup(name="girc",
-      version="0.0.1",
-      author="ekimekim",
-      author_email="mikelang3000@gmail.com",
-      description="gevent based irc library",
-      packages=find_packages(),
-      install_requires=[
-          'gevent',
-          'monotonic',
-      ],
-     )
+setup(
+	name="girc",
+	version="0.0.1",
+	author="ekimekim",
+	author_email="mikelang3000@gmail.com",
+	description="gevent based irc library",
+	packages=find_packages(),
+	install_requires=[
+		'gevent',
+		'monotonic',
+	],
+)
