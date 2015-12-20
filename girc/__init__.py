7a616cfb6e2f297756d888fc550ef99ecff19365
Mike Lang <mikelang3000@gmail.com>
2015-12-20 06:11:04 -0800

    Import channel from __init__

--- girc/__init__.py
+++ girc/__init__.py
@@ -1,4 +1,5 @@
 from girc.client import Client
+from girc.channel import Channel
 from girc.handler import Handler
 from girc.replycodes import replies, errors
 from girc.message import Message, Nick, User, Quit, Join, Part, Mode, Privmsg, List, Kick, Whois, \
