03898e66e6b7883516b0a356f6c15b07f8b069e7
Mike Lang <mikelang3000@gmail.com>
2016-12-11 21:15:44 -0800

    Release 1.0.0

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
 	name="girc",
-	version="0.0.1",
+	version="1.0.0",
 	author="ekimekim",
 	author_email="mikelang3000@gmail.com",
 	description="gevent based irc library",
+	url="https://github.com/ekimekim/girc",
+	download_url = 'https://github.com/ekimekim/girc/tarball/1.0.0',
 	packages=find_packages(),
 	install_requires=[
 		'gevent',
